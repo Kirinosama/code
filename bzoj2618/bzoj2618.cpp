560adc5f87a44dd338aefa6221674b831ced3390
wqs <506445435@qq.com>
2018-01-19 15:47:08 +0800

    update

--- bzoj2618/bzoj2618.cpp
+++ bzoj2618/bzoj2618.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <queue>
 using namespace std;
 
 #define eps 1e-8
@@ -13,15 +14,15 @@ using namespace std;
 typedef long long ll;
 
 struct node{
-	int x,y;
+	double x,y;
 	void in(){scanf("%d%d",&x,&y);}
 	node operator -(const node &a){
-		return (node){y-a.y,x-a.x};
+		return (node){x-a.x,y-a.y};
 	}
 	double operator *(const node &a){
 		return x*a.y-y*a.x;
 	}
-}p[MAXN];
+}p[MAXN],ans[MAXN];
 
 struct line{
 	node a,b;
@@ -30,7 +31,9 @@ struct line{
 		a=p,b=q;
 		ang=atan2(b.y-a.y,b.x-a.x);
 	}
-}l[MAXN];
+}l[MAXN],Q[MAXN*2];
+
+int n,m,cnt,anscnt;
 
 int comp(double a,double b){
 	if(fabs(a-b)<eps) return 0;
@@ -40,7 +43,7 @@ int comp(double a,double b){
 
 int isleft(line a,node b){
 	double res=(a.a-b)*(a.b-b);
-	if(abs(res)<eps) return 0;
+	if(fabs(res)<eps) return 0;
 	else if(res>0) return 1;
 	else return -1;
 }
@@ -51,7 +54,37 @@ bool cmp(const line &a,const line &b){
 	return a.ang<b.ang;
 }
 
-int n,m,cnt;
+node getintersect(line a,line b){
+	node cur;
+	double A1=a.a.y-a.b.y,B1=a.b.x-a.a.x,C1=a.a*a.b;
+	double A2=b.a.y-b.b.y,B2=b.b.x-b.a.x,C2=b.a*b.b;
+	double d=A1*B2-A2*B1;
+	cur.x=(double)(B1*C2-B2*C1)/d;
+	cur.y=(double)(A2*C1-A1*C2)/d;
+	return cur;
+}
+
+bool judge(line a,line b,line c){
+	node cur=getintersect(b,c);
+	return isleft(a,cur)<=0;
+}
+
+void solve(){
+	int head=1,tail=2;
+	Q[1]=l[1],Q[2]=l[2];
+	for(int i=3;i<=cnt;i++){
+		while(head<tail && judge(l[i],Q[tail],Q[tail-1])) tail--;
+		while(head<tail && judge(l[i],Q[head],Q[head+1])) head++;
+		Q[++tail]=l[i];
+	}
+	while(head<tail && judge(Q[head],Q[tail],Q[tail-1])) tail--;
+	while(head<tail && judge(Q[tail],Q[head],Q[head+1])) head++;
+	Q[++tail]=Q[head];
+	for(int i=head;i<tail;i++){
+		ans[++anscnt]=getintersect(Q[i],Q[i+1]);
+		printf("(%d,%d)\n",ans[anscnt].x,ans[anscnt].y);
+	}
+}
 
 int main(){
     freopen("bzoj2618.in","r",stdin);
@@ -67,7 +100,6 @@ int main(){
     for(int i=1;i<=cnt;i++)
     	if(comp(l[i].ang,l[cnt1].ang)) l[++cnt1]=l[i];
  	cnt=cnt1;
- 	for(int i=1;i<=cnt;i++)
- 		printf("(%d,%d) (%d,%d)\n",l[i].a.x,l[i].a.y,l[i].b.x,l[i].b.y);
+ 	solve();
     return 0;
 }
