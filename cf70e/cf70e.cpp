564331b7383139909b6d8bb9ca95d9426d9c7f13
wqs <506445435@qq.com>
2018-04-10 16:31:19 +0800

    a

---
+++ cf70e/cf70e.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define ii pair<int,int>
+#define INF 0x3f3f3f3f
+typedef long long ll;
+
+int main(){
+    freopen("cf70e.in","r",stdin);
+    cin>>q;q-=3;
+    premake();
+    return 0;
+}
