71d85a3454803cdd60011d80b86e772f3254958c
wqs <506445435@qq.com>
2018-04-10 22:34:36 +0800

    update

---
+++ education42/e.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+#define ii pair<int,int>
+#define INF 0x3f3f3f3f
+
+int main(){
+	freopen("input","r",stdin);
+
+	return 0;
+}
