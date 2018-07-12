3ebd5e199ae8c55ff63d43b6cc6e02aa6d9f90d3
Kirinosama <506445435@qq.com>
2018-07-12 19:57:30 +0800

    a

--- 123/123.cpp
+++ 123/123.cpp
@@ -1,17 +1,11 @@
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ii pair<int,int>
 #define INF 0x3f3f3f3f
+#define ii pair<int,int>
 typedef long long ll;
 
 int main(){
-    freopen("123.in","r",stdin);
-    printf("hello world");
-    printf("nice!");
-    return 0;
+	freopen("123.in","r",stdin);
+	return 0;
 }
